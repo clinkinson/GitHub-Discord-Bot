90093148d23728dac92937be87cc293b55ed2c6b
Luis Ramirez <lgr1@sfu.ca>
2025-11-15 21:37:22 -0800

    Updated languages

--- app.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
-
-from gdb.bot import run_bot
-
-if __name__ == "__main__":
-    run_bot()
--- src/gdb/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-GUILD_ID = int(os.getenv("GUILD_ID", 0))
